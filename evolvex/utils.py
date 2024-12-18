cd3100002d71edcd0943c4ae52338c2991c3a604
gciaberi <49762942+gciaberi@users.noreply.github.com>
2024-12-18 10:43:36 +0100

    Delete evolvex directory

--- evolvex/utils.py
+++
@@ -1,9 +0,0 @@
-
-import tarfile
-
-NDIGIS_ROUNDING = 4
-
-def save_compressed_PDB_file(PDB_file_path, output_name, output_dir):
-    with tarfile.open(output_dir / f'{output_name}.tar.gz', 'w:gz') as tar_file_handle:
-        tar_file_handle.add(PDB_file_path, arcname=output_name)
-    return
