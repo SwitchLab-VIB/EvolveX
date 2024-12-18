b89b984d9a46141211bb305fba91bef2b589db03
gciaberi <49762942+gciaberi@users.noreply.github.com>
2024-12-18 10:39:56 +0100

    Update setup.py file

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     python_requires='>=3.9',
-    install_requires=['pyyaml', 'biopython', 'pandas', 'dask', 'distributed', 'dask-jobqueue'],
+    install_requires=['pyyaml', 'biopython>=1.81', 'pandas', 'dask', 'distributed', 'dask-jobqueue'],
     entry_points={
         'console_scripts': [
             'evolvex = evolvex.main:main',
