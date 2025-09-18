09c488b8fc62733af14f30be77e2dc922df9ffb3
Zhongyao <90295301+Zhongyao1995@users.noreply.github.com>
2025-09-18 18:18:04 +0200

    Add files via upload

---
+++ NGS/FoldX_analysis/NGS_sequence_modeling/CDR2/NGS_AC_cdr2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+./foldx -f config_AC_5imk_cdr2.cfg
+./foldx -f config_AC_5iml_cdr2.cfg
+./foldx -f config_AC_5imm_cdr2.cfg
+./foldx -f config_AC_5imo_cdr2.cfg
+./foldx -f config_AC_9ezh_cdr2.cfg
+./foldx -f config_AC_9ezi_cdr2.cfg
+./foldx -f config_AC_9ezu_cdr2.cfg
+./foldx -f config_AC_9ezv_cdr2.cfg
+./foldx -f config_AC_9ezw_cdr2.cfg
---
+++ NGS/FoldX_analysis/NGS_sequence_modeling/CDR2/NGS_BM_cdr2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+./foldx -f config_BM_5imk_cdr2.cfg
+./foldx -f config_BM_5iml_cdr2.cfg
+./foldx -f config_BM_5imm_cdr2.cfg
+./foldx -f config_BM_5imo_cdr2.cfg
+./foldx -f config_BM_9ezh_cdr2.cfg
+./foldx -f config_BM_9ezi_cdr2.cfg
+./foldx -f config_BM_9ezu_cdr2.cfg
+./foldx -f config_BM_9ezv_cdr2.cfg
+./foldx -f config_BM_9ezw_cdr2.cfg
