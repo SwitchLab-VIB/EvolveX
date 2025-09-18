fff54b8546033cdc4d923c83c9524b423522c24f
Zhongyao <90295301+Zhongyao1995@users.noreply.github.com>
2025-09-18 18:12:46 +0200

    Add files via upload

---
+++ NGS/FoldX_analysis/Ala_starting/CDR2/Ala_BM_cdr2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+./foldx -f config_BM_5imk_cdr2_ala.cfg
+./foldx -f config_BM_5iml_cdr2_ala.cfg
+./foldx -f config_BM_5imm_cdr2_ala.cfg
+./foldx -f config_BM_5imo_cdr2_ala.cfg
+./foldx -f config_BM_9ezh_cdr2_ala.cfg
+./foldx -f config_BM_9ezi_cdr2_ala.cfg
+./foldx -f config_BM_9ezu_cdr2_ala.cfg
+./foldx -f config_BM_9ezv_cdr2_ala.cfg
+./foldx -f config_BM_9ezw_cdr2_ala.cfg
